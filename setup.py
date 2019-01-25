ffe53aa552bd06d66415a11e090f5dc7ac222734
victory <33646490+Aurorauo@users.noreply.github.com>
2019-01-25 18:27:34 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gain_data',
+      version='0.0.1',
+      description='Simplify your data acquisition process dramatically',
+      url='https://github.com/Aurorauo/gain_data',
+      author='zf',
+      author_email='zf083415@gmail.com',
+      license='MIT',
+      packages=['gain_data'])
